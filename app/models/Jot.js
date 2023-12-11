f318d4a7f33515cfb650869ed438f0f20aa7093c
Kaden <kjrigsbee@gmail.com>
2023-12-10 21:16:26 -0700

    we cookin

--- app/models/Jot.js
+++ app/models/Jot.js
@@ -20,7 +20,7 @@ export class Jot {
     const formattedDate = this.formattedCreatedDate();
     return `
       <div class="text-dark d-flex justify-content-between">
-        <span>${this.name}</span>
+        <span>${this.name}<i class="mdi mdi-circle"></i></span>
         <span>${formattedDate}</span>
         <span>
           <button onclick="app.JotController.openJot('${this.id}')" class="btn btn-outline-dark" title="open Jot"><i class="mdi mdi-folder-open"></i></button>
@@ -32,9 +32,16 @@ export class Jot {
   get ActiveNoteTemplate(){
     const formattedDate = this.formattedCreatedDate();
     return `
-      <div class="col-12">
-        <h1 class="text-uppercase fw-bold">${this.name} ${formattedDate}</h1>
-        <p>${this.body}</p>
+      <div class="row bg-dark">
+        <div class="col-4 text-light">
+          <h4 class="text-uppercase fw-bold">${this.name} <i class="mdi mdi-circle"></i></h4>
+          <h6>Created at ${formattedDate}</h6>
+          <h6>Updated at updatedDate</h6>
+        </div>
+        <div class="col-8">
+          <textarea rows="20" class="w-100" id="active-case-body" maxlength=2000>${this.body}</textarea>
+        </div>
+        <button onclick="app.JotController.saveJot()" class="btn btn-danger">save Jot<i class="mdi mdi-content-save"></i></button>
       </div>
     `
   }
